49df6e2d2a9ec5f301f2f29bb2c2ef67a152645c
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-11-07 11:30:03 +0000

    Wrote another IF condition that prints 'Buzz' when the number is divisible by ONLY 5

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,12 +1,12 @@
 function FizzBuzz(n) {
     // If the number is divisible by 3 and 5, return 'FizzBuzz'
-    if (n % 3 === 0) {
+    if (n % 3 === 0 && n % 5 === 0) {
         return 'FizzBuzz';
+    }
+    else if (n % 5 === 0) {
+        return 'Buzz';
     }
 }
 
-// Made to pass
-console.log(FizzBuzz(6));
-// Made to fail
-console.log(FizzBuzz(97));
+console.log(FizzBuzz(20));
 
