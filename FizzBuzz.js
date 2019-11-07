bbccb8c35fdc10e6c3878dc7699d198b21c1f109
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-11-07 11:50:44 +0000

    Returns Fizz

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -3,10 +3,17 @@ function FizzBuzz(n) {
     if (n % 3 === 0 && n % 5 === 0) {
         return 'FizzBuzz';
     }
+    // Returns buzz if the number is divisible by ONLY 5
     else if (n % 5 === 0) {
         return 'Buzz';
     }
+    // Returns buzz if the number is divisible by ONLY 3
+    else if (n % 3 === 0) {
+        return 'Fizz';
+    } else {
+        return n;
+    }
 }
 
-console.log(FizzBuzz(20));
+console.log(FizzBuzz(30));
 
